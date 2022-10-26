3c77a20fda7d61a7851c6854bdbcb07411708ffb
Luis Francisco Becerril Jurado <99098643+LuisFBecerrilJurado@users.noreply.github.com>
2022-10-26 15:46:10 -0500

    Añadiendo estado mostrar y ocultar sección inicio

--- js/menu.js
+++ js/menu.js
@@ -1,14 +1,20 @@
-const toggleButton = document.getElementById('button-menu')
-const navWrapper = document.getElementById('nav')
+const toggleButton = document.getElementById("button-menu");
+const navWrapper = document.getElementById("nav");
+const inicio = document.getElementById("inicio");
+const list = document.getElementById("menulist");
+const clientes = document.getElementById("clientes");
+const contacto = document.getElementById("contacto");
 
-toggleButton.addEventListener('click',() => {
-  toggleButton.classList.toggle('close')
-  navWrapper.classList.toggle('show')
-})
+toggleButton.addEventListener("click", () => {
+  toggleButton.classList.toggle("close");
+  navWrapper.classList.toggle("show");
+  inicio.classList.toggle("ocultar");
+});
 
-navWrapper.addEventListener('click',e => {
-  if(e.target.id === 'nav'){
-    navWrapper.classList.remove('show')
-    toggleButton.classList.remove('close')
+navWrapper.addEventListener("click", (e) => {
+  if (e.target.id === "nav") {
+    navWrapper.classList.remove("show");
+    toggleButton.classList.remove("close");
+    inicio.classList.remove("show");
   }
-})
+});

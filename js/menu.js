e06c7e650edeef9e5bf176802a699dfe50f5a548
Luis Francisco Becerril Jurado <99098643+LuisFBecerrilJurado@users.noreply.github.com>
2022-10-26 21:26:45 -0500

    Añadiendo seccion de menu

--- js/menu.js
+++ js/menu.js
@@ -1,20 +1,17 @@
 const toggleButton = document.getElementById("button-menu");
 const navWrapper = document.getElementById("nav");
 const inicio = document.getElementById("inicio");
-const list = document.getElementById("menulist");
-const clientes = document.getElementById("clientes");
-const contacto = document.getElementById("contacto");
 
 toggleButton.addEventListener("click", () => {
   toggleButton.classList.toggle("close");
   navWrapper.classList.toggle("show");
-  inicio.classList.toggle("ocultar");
+  inicio.style.visibility = hidden;
 });
 
 navWrapper.addEventListener("click", (e) => {
   if (e.target.id === "nav") {
     navWrapper.classList.remove("show");
     toggleButton.classList.remove("close");
-    inicio.classList.remove("show");
+    inicio.style.visibility = visible;
   }
 });
